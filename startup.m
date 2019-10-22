3e2463e07cc12b1a94fd88a97935e3c7a63c27a3
Vedant Puri <vpuri3@illinois.edu>
2019-10-22 15:37:30 -0500

    latex

--- startup.m
+++ startup.m
@@ -1,3 +1,12 @@
 %
 addpath('~/matlab/spec/plt');
+addpath('~/matlab/spec/sem/semhat');
 addpath('~/Nek5000/run/NekRoutines/mtlb');
+addpath('~/Nek5000/run/NekRoutines/mtlb');
+
+set(groot, 'DefaultAxesFontSize',14);
+set(groot,'DefaultTextInterpreter'             ,'latex');
+set(groot,'DefaultAxesTickLabelInterpreter'    ,'latex');
+set(groot,'DefaultLegendInterpreter'           ,'latex');
+set(groot,'DefaultColorbarTickLabelInterpreter','latex');
+
