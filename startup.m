f40e3beb72377e6eea821e6409a9536cba2d7397
Vedant Puri <vpuri3@illinois.edu>
2019-12-08 18:24:01 -0600

    cleanup

--- startup.m
+++ startup.m
@@ -6,7 +6,7 @@ addpath('~/matlab/spec/geom');
 addpath('~/matlab/spec/tstep');
 addpath('~/Nek5000/run/NekRoutines/mtlb');
 
-set(groot, 'DefaultAxesFontSize',14);
+set(groot,'DefaultAxesFontSize',14);
 set(groot,'DefaultTextInterpreter'             ,'latex');
 set(groot,'DefaultAxesTickLabelInterpreter'    ,'latex');
 set(groot,'DefaultLegendInterpreter'           ,'latex');
