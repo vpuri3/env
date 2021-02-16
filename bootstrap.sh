a6abb00fbc217c8229b889ac34a8ebe3ad7f9d07
Vedant Puri <vedantpuri@gmail.com>
2021-02-16 12:54:43 -0600

    reorder

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,29 +2,8 @@
 
 cd $HOME
 
-[ ! -d ~/env ] && git clone https://github.com/vpuri3/env.git
-
-touch ~/.bash_profile
-
-echo "# https://github.com/vpuri3/env/blob/master/bootstrap.sh">>~/.bash_profile
-echo "[ -f ~/.bashrc ] && source ~/.bashrc"                    >>~/.bash_profile
-echo "source ~/env/bash_vars"                                  >>~/.bash_profile
-echo "source ~/env/bash_alias"                                 >>~/.bash_profile
-
-source ~/.bash_profile
-
-ln -sf ~/env/emacs.conf  ~/.emacs
-ln -sf ~/env/vimrc       ~/.vimrc
-ln -sf ~/env/gitconfig   ~/.gitconfig
-ln -sf ~/env/tmux.conf   ~/.tmux.conf
-
-ln -sf ~/env/bin ~/bin
-chmod +x ~/bin/*
-
 case `uname` in
 Darwin)
-	#echo "python = python3"
-	#ln -sf $(which python3) /usr/local/bin/python
 	echo "xcode"
 	xcode-select -p > /dev/null
 	[ "$?" == "0" ] || xcode-select --install
@@ -47,6 +26,25 @@ Linux)
 	sudo apt-get install git unzip wget
 	;;
 esac
+
+[ ! -d ~/env ] && git clone https://github.com/vpuri3/env.git
+
+touch ~/.bash_profile
+
+echo "# https://github.com/vpuri3/env/blob/master/bootstrap.sh">>~/.bash_profile
+echo "[ -f ~/.bashrc ] && source ~/.bashrc"                    >>~/.bash_profile
+echo "source ~/env/bash_vars"                                  >>~/.bash_profile
+echo "source ~/env/bash_alias"                                 >>~/.bash_profile
+
+source ~/.bash_profile
+
+ln -sf ~/env/emacs.conf  ~/.emacs
+ln -sf ~/env/vimrc       ~/.vimrc
+ln -sf ~/env/gitconfig   ~/.gitconfig
+ln -sf ~/env/tmux.conf   ~/.tmux.conf
+
+ln -sf ~/env/bin ~/bin
+chmod +x ~/bin/*
 
 cd $HOME
 
