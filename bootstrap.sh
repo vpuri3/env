bba43bdad0e2916e16ffb312a10dbffb951c261e
Vedant Puri <vedantpuri@gmail.com>
2023-02-21 12:14:11 -0500

    gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,6 +38,7 @@ mkdir -p $HOME/.ssh
 ln -sf $HOME/env/emacs.conf $HOME/.emacs
 ln -sf $HOME/env/vimrc      $HOME/.vimrc
 ln -sf $HOME/env/gitconfig  $HOME/.gitconfig
+ln -sf $HOME/env/gitignore  $HOME/.gitignore
 ln -sf $HOME/env/tmux.conf  $HOME/.tmux.conf
 ln -sf $HOME/env/sshconfig  $HOME/.ssh/config
 
