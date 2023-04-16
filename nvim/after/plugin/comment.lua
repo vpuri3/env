e85070bd4818509e1cfc1bdc7513ad73f0d7ad9a
Vedant Puri <vedantpuri@gmail.com>
2023-04-16 16:01:05 -0400

    fix comment typo

--- nvim/after/plugin/comment.lua
+++ nvim/after/plugin/comment.lua
@@ -3,7 +3,7 @@ require('nvim_comment').setup({
     -- Should key mappings be created
     create_mappings = true,
     -- Normal mode mapping left hand side
-    line_mapping = "<leader>c",
+    line_mapping = "<leader>cc",
     -- Visual/Operator mapping left hand side
     operator_mapping = "<leader>c",
 })
