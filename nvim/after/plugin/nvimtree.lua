0f0807ac6fc99f24cf562fafc13697b8c31a782e
Vedant Puri <vedantpuri@gmail.com>
2023-04-16 11:30:42 -0400

    icon

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -63,6 +63,29 @@ require("nvim-tree").setup({
                 folder_arrow = true,
                 git = true,
             },
+            glyphs = {
+                default = "",
+                symlink = "",
+                folder = {
+                    default = "",
+                    empty = "",
+                    empty_open = "",
+                    open = "",
+                    symlink = "",
+                    symlink_open = "",
+                    arrow_open = "",
+                    arrow_closed = "",
+                },
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌",
+                },
+            }
         }
     }
 })
